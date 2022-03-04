56b61e72d395fe3d633bb02b09b43225e7175a65
Samuel Wölfl <samuelwoelfl@gmx.de>
2022-03-04 22:53:43 +0100

    simple design and first queries

--- js/form.js
+++ js/form.js
@@ -1,26 +1,81 @@
-var origin = 'Freiburg';
-var destination = 'Stuttgart';
+var origin = 'Burghaldenweg 64 Stuttgart';
+var destination = 'Stuttgart Universität Vaihingen';
+var arrivalTime = new Date(new Date().getTime() + 30 * 60000);
 
+var routes;
 
 $(window).on("load", function() {
-  var service = new google.maps.DistanceMatrixService;
-  response = service.getDistanceMatrix({
-    origins: [origin],
-    destinations: [destination],
+  var service = new google.maps.DirectionsService;
+  service.route({
+    origin: origin,
+    destination: destination,
     travelMode: 'TRANSIT',
+    provideRouteAlternatives: true,
     unitSystem: google.maps.UnitSystem.METRIC,
-    avoidHighways: false,
-    avoidTolls: false
     transitOptions: {
-      arrivalTime: date.now() + 3,
+      arrivalTime: arrivalTime,
       // departureTime: date.now(),
-      modes[]: TransitMode,
-      routingPreference: TransitRoutePreference
     },
-    unitSystem: google.maps.UnitSystem.IMPERIAL
+  }, function(response, status) {
+    if (status == "OK") {
+      console.log(response["routes"]);
+      routes = response["routes"];
+      printRoutes(routes);
+    }
   });
-  console.log(response);
 });
+
+
+function printRoutes(routes) {
+  for (var i in routes) {
+    route = routes[i];
+    leg = route["legs"][0];
+    var legClean = {
+      "start": leg["start_address"],
+      "end": leg["end_address"],
+      "departure": leg["departure_time"]["value"],
+      "arrival": leg["arrival_time"]["value"],
+      "duration": leg["duration"]["value"],
+    }
+
+    stepsRaw = leg["steps"];
+    stepsClean = [];
+    for (var i in stepsRaw) {
+      stepRaw = stepsRaw[i];
+
+      var step = {}
+      step["type"] = stepRaw["travel_mode"];
+      step["description"] = stepRaw["instructions"];
+      step["stepDuration"] = stepRaw["duration"]["value"];
+      step["stepDistance"] = stepRaw["distance"]["value"];
+      if (step["type"] == "TRANSIT") {
+        step["line"] = stepRaw["transit"]["line"]["short_name"];
+        step["vehicleName"] = stepRaw["transit"]["line"]["vehicle"]["name"];
+        step["headsign"] = stepRaw["transit"]["headsign"];
+        step["stepEnd"] = stepRaw["transit"]["arrival_stop"]["name"];
+        step["stepArrival"] = stepRaw["transit"]["arrival_time"]["value"];
+        step["stepStart"] = stepRaw["transit"]["departure_stop"]["name"];
+        step["stepDeparture"] = stepRaw["transit"]["departure_time"]["value"];
+      } else if (step["type"] == "WALKING") {
+        if (i == 0) {
+          step["stepDeparture"] = legClean["departure"];
+        } else {
+          step["stepDeparture"] = stepsRaw[i - 1]["transit"]["arrival_time"]["value"];
+        }
+        step["stepArrival"] = Date(Date(step["stepDeparture"]) + step["stepDuration"]);
+      }
+      stepsClean.push(step);
+      // console.log(step);
+    }
+    legClean["steps"] = stepsClean;
+    console.log(legClean);
+
+    // console.log(`Verbindung ${start} -> ${end} :\n\nAbfahrt: ${departure}\nAnkunft: ${arrival}\nDauer:${duration}\n  \n\n`);
+  }
+}
+
+
+
 
 function initMap() {
   console.log('test');
---
+++ js/response.js
@@ -0,0 +1,190 @@
+{
+  "bounds": {
+    "south": 47.99774000000001,
+    "west": 7.761240000000001,
+    "north": 49.0082,
+    "east": 9.19485
+  },
+  "copyrights": "Map data ©2022 GeoBasis-DE/BKG (©2009), Google",
+  "legs": [{
+    "arrival_time": {
+      "text": "22:02",
+      "time_zone": "Europe/Berlin",
+      "value": "2022-03-04T21:02:00.000Z"
+    },
+    "departure_time": {
+      "text": "19:55",
+      "time_zone": "Europe/Berlin",
+      "value": "2022-03-04T18:55:30.000Z"
+    },
+    "distance": {
+      "text": "213 km",
+      "value": 213124
+    },
+    "duration": {
+      "text": "2 Stunden, 7 Minuten",
+      "value": 7590
+    },
+    "end_address": "Stuttgart, Deutschland",
+    "end_location": {
+      "lat": 48.7866097,
+      "lng": 9.183259
+    },
+    "start_address": "Freiburg im Breisgau, Deutschland",
+    "start_location": {
+      "lat": 47.99773769999999,
+      "lng": 7.841504400000001
+    },
+    "steps": [{
+        "distance": {
+          "text": "132 km",
+          "value": 131888
+        },
+        "duration": {
+          "text": "1 Stunde, 4 Minuten",
+          "value": 3840
+        },
+        "end_location": {
+          "lat": 48.9938334,
+          "lng": 8.4019659
+        },
+        "polyline": {},
+        "start_location": {
+          "lat": 47.99773769999999,
+          "lng": 7.841504400000001
+        },
+        "travel_mode": "TRANSIT",
+        "instructions": "Hochgeschwindigkeitszug in Richtung Dortmund Hbf",
+        "maneuver": "",
+        "start_point": {
+          "lat": 47.99773769999999,
+          "lng": 7.841504400000001
+        },
+        "end_point": {
+          "lat": 48.9938334,
+          "lng": 8.4019659
+        },
+        "transit": {
+          "arrival_stop": {
+            "location": {
+              "lat": 48.9938334,
+              "lng": 8.4019659
+            },
+            "name": "Karlsruhe Hbf"
+          },
+          "arrival_time": {
+            "text": "20:57",
+            "time_zone": "Europe/Berlin",
+            "value": "2022-03-04T19:57:20.000Z"
+          },
+          "departure_stop": {
+            "location": {
+              "lat": 47.99773769999999,
+              "lng": 7.841504400000001
+            },
+            "name": "Freiburg(Breisgau) Hbf"
+          },
+          "departure_time": {
+            "text": "19:55",
+            "time_zone": "Europe/Berlin",
+            "value": "2022-03-04T18:55:30.000Z"
+          },
+          "headsign": "Dortmund Hbf",
+          "line": {
+            "agencies": [{
+              "name": "DB Fernverkehr AG",
+              "url": "https://www.bahn.de/"
+            }],
+            "color": "#f01414",
+            "short_name": "ICE100",
+            "text_color": "#ffffff",
+            "vehicle": {
+              "icon": "https://maps.gstatic.com/mapfiles/transit/iw2/6/rail2.png",
+              "name": "Hochgeschwindigkeitszug",
+              "type": "HIGH_SPEED_TRAIN"
+            }
+          },
+          "num_stops": 3
+        }
+      },
+      {
+        "distance": {
+          "text": "81,2 km",
+          "value": 81236
+        },
+        "duration": {
+          "text": "56 Minuten",
+          "value": 3360
+        },
+        "end_location": {
+          "lat": 48.7866097,
+          "lng": 9.183259
+        },
+
+        "start_location": {
+          "lat": 48.993226,
+          "lng": 8.402182
+        },
+        "travel_mode": "TRANSIT",
+        "instructions": "Zug in Richtung Stuttgart Hbf",
+        "maneuver": "",
+        "start_point": {
+          "lat": 48.993226,
+          "lng": 8.402182
+        },
+        "end_point": {
+          "lat": 48.7866097,
+          "lng": 9.183259
+        },
+        "transit": {
+          "arrival_stop": {
+            "location": {
+              "lat": 48.7866097,
+              "lng": 9.183259
+            },
+            "name": "Stuttgart Hbf"
+          },
+          "arrival_time": {
+            "text": "22:02",
+            "time_zone": "Europe/Berlin",
+            "value": "2022-03-04T21:02:00.000Z"
+          },
+          "departure_stop": {
+            "location": {
+              "lat": 48.993226,
+              "lng": 8.402182
+            },
+            "name": "Karlsruhe Hbf"
+          },
+          "departure_time": {
+            "text": "21:06",
+            "time_zone": "Europe/Berlin",
+            "value": "2022-03-04T20:06:32.000Z"
+          },
+          "headsign": "Stuttgart Hbf",
+          "line": {
+            "agencies": [{
+              "name": "DB Fernverkehr AG",
+              "url": "https://www.bahn.de/"
+            }],
+            "color": "#f01414",
+            "short_name": "IC2167",
+            "text_color": "#ffffff",
+            "vehicle": {
+              "icon": "https://maps.gstatic.com/mapfiles/transit/iw2/6/rail2.png",
+              "name": "Zug",
+              "type": "HEAVY_RAIL"
+            }
+          },
+          "num_stops": 4
+        }
+      }
+    ],
+    "traffic_speed_entry": [],
+    "via_waypoint": [],
+    "via_waypoints": []
+  }],
+  "summary": "",
+  "warnings": [],
+  "waypoint_order": [],
+}
